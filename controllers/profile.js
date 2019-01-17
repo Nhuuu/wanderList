ce08425fdebf4c8bd866bafced47f90f0aa640e5
Nhuuu <nqtrinh87@gmail.com>
2019-01-16 17:49:38 -0800

    delete places and show routes are done.

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,14 +1,15 @@
 var db = require('../models');
 var express = require('express');
+var methodOverride = require('method-override');
 var router = express.Router();
-
-// Include ref to middleware > loggedIn.js
 var loggedIn = require('../middleware/loggedIn');
+// var mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+// var geocodingClient = mbxGeocoding({accessToken: process.env.geocodeToken});
 
 
+router.use(methodOverride('_method'));
 
-// GET all user's places 
-// Stretch goal, include Mapbox & markers.
+// GET all user's places >> include geocode map here
 router.get('/', loggedIn, (req, res) => {
 	db.place.findAll()
 	.then((allPlaces) => {
@@ -31,20 +32,20 @@ router.get('/', loggedIn, (req, res) => {
 
 
 
-// GET show route for 1 user's place
-// router.get('/:id', (req, res) => {
-// 	db.place.find({
-// 		where: {id: req.params.id}
-// 	})
-// 	.then((place) =>{
-// 		if(!place) throw Error();
-// 		res.render('show', { place: place })
-// 	})
-// 	.catch((err) =>{
-// 		res.render('error')
-// 		console.log(err)
-// 	})
-// })
+// GET show route for 1 user's place, points of interests associated + notes.
+router.get('/show/:id', (req, res) => {
+	db.place.find({
+		where: {id: req.params.id}
+	})
+	.then((place) =>{
+		if(!place) throw Error();
+		res.render('show', { place: place })
+	})
+	.catch((err) =>{
+		res.render('error')
+		console.log(err)
+	})
+})
 
 
 // POST show route for notes functionality/association with place.
@@ -54,24 +55,24 @@ router.get('/', loggedIn, (req, res) => {
 
 
 // Delete route for places.
-// router.delete('/:id', (req, res) => {
-// 	db.place.destroy({
-// 		where: {id: req.params.id}
-// 	})
-// 	.then((deletedPlace) => {
-// 		db.placeUser.destroy({
-// 			where: {cityId: req.params.id}
-// 		})
-// 		.then((deletedAssociation) => {
-// 			res.redirect('/profile');
-// 		})
-// 	})
-// })
+router.delete('/:id', (req, res) => {
+	db.place.destroy({
+		where: {id: req.params.id}
+	})
+	.then((deletedPlace) => {
+		db.placeUser.destroy({
+			where: {placeId: req.params.id}
+		})
+		.then((deletedAssociation) => {
+			res.redirect('/profile');
+		})
+	})
+})
 
 
 // Delete route for attractions from places
 // router.delete('/association', (req, res) => {
-// 	db.attractionPlace.destroy({
+// 	db.poiPlace.destroy({
 // 		where: {
 // 			placeId: req.body.placeId,
 // 			attractionId: req.body.attractionId
