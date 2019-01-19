fcd277b6dc8ff41c1439123e3805877c02a7c56c
Nhuuu <nqtrinh87@gmail.com>
2019-01-18 16:41:55 -0800

    add place button disappears/poi button appears when place is added

--- controllers/search.js
+++ controllers/search.js
@@ -14,17 +14,15 @@ router.get('/', loggedIn, (req, res) => {
 	res.render('search-results', { placeDetails: '', photos: [], placeId: '' });
 })
 
-// GET search route, get location results - display photo and 20 attractions to add.
+// GET search route, get location results - display photos and 20 attractions to add.
 router.get('/results', (req, res) => {
 	console.log(req.query)
 	var placeUrl = 'https://maps.googleapis.com/maps/api/place/autocomplete/json?input='+req.query.search+'&types=geocode&political&key='+process.env.googleKey;
 	request(placeUrl, (err, response, placeBody) => {
-		console.log("INSIDE FIRST REQUEST")
 	 	var placeDetails = JSON.parse(placeBody).predictions[0];
 	 	var placeDetailsId = placeDetails.place_id;
 	 	var placeIdUrl = 'https://maps.googleapis.com/maps/api/place/details/json?placeid='+placeDetailsId+'&key='+process.env.googleKey;
 	 	request(placeIdUrl, (err, response, body) => {
-	 		console.log('INSIDE SECOND REQUEST')
 	 		var latLng = JSON.parse(body).result.geometry.location;
 	 		var photosArr = JSON.parse(body).result.photos || [];
 	 		var photoRefArr = photosArr.map(ref => ref.photo_reference);
@@ -43,6 +41,9 @@ router.get('/results', (req, res) => {
 					var results = data.jsonBody.businesses;
 					res.render('search-results', { placeDetails: placeDetails, latLng: latLng, photos: photosArr, results: results, place: place });
 				})
+				.catch((err) => {
+					console.log('error getting place database info');
+				})
 			})
 			.catch((err) => {
 				console.log(err);
@@ -52,7 +53,7 @@ router.get('/results', (req, res) => {
 	});
 });
 
-// POST route to add a place to a user's profile. 
+// POST route to add place to a user's profile. 
 router.post('/add', (req, res) => {
 	db.place.findOrCreate({
 		where: {
@@ -70,7 +71,6 @@ router.post('/add', (req, res) => {
 			place.addUser(user)
 			.then((user) => {
 				console.log('association happened for user to place');
-				console.log("place.description!!!!!", place.description);
 				res.redirect('/search/results?search='+place.description.toLowerCase());
 			})
 			.catch((err) => {
@@ -87,7 +87,7 @@ router.post('/add', (req, res) => {
 	});
 });		
 
-// POST route to add points of interest.
+// POST route to add points of interest to place.
 router.post('/add-poi', (req, res) => {
 	console.log("REQ.BODY.placeId", req.body.placeId)
 	db.place.findOne({
